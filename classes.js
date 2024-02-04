d3eeef31d062ce2710f1a83a5643dbd3c461f0bc
ChimkariweObuseh <145494543+ChimkariweObuseh@users.noreply.github.com>
2024-02-04 12:18:06 +0000

    Update classes.js

--- classes.js
+++ classes.js
@@ -149,8 +149,8 @@ class Car {
 class Sensor {
     constructor(car) {
         this.car = car;
-        this.rayCount = 15;
-        this.rayLength = 300;
+        this.rayCount = 5;
+        this.rayLength = 150;
         this.raySpread = Math.PI / 2;
 
         this.rays = [];
